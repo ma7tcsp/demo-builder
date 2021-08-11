6a86feb9d76141f4669bca74fd65037be3cc275f
aalteirac <anthony@alteirac.com>
2021-08-11 14:02:02 +0200

    auto-build

--- build-it.sh
+++ build-it.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+git add .
+git commit -m "auto-build"
+git push
 docker stop testemb 
 docker rm testemb 
 docker image rm alteirac/emb
 docker build . -t alteirac/emb
 docker run -p 80:3000 -d --name testemb alteirac/emb
-#docker push alteirac/emb
+docker push alteirac/emb
 
