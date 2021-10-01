6d453c09a412c9c9fa02c8d92f5f38a27790a39b
aalteirac <anthony@alteirac.com>
2021-10-01 23:33:19 +0200

    v1.21, divider menu style

--- build-it.sh
+++ build-it.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 node prepdeploy.js
 git add .
-git commit -m "v1.2, New Vertical Template !"
+git commit -m "v1.21, divider menu style"
 git push
 docker stop testemb 
 docker rm testemb 
