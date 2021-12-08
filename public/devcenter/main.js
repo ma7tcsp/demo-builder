2f33f241a5eef4e5313f9729a79bd4a771092527
aalteirac <anthony@alteirac.com>
2021-12-09 00:56:36 +0100

    v1.3.3, widget beta almost ready, need regression test

--- public/devcenter/main.js
+++ public/devcenter/main.js
@@ -1201,7 +1201,7 @@ function saveTemplateSettings(close){
     restoreTexts();
   })
   $("#viewlist .filterText").each((index,el)=>{
-    console.log("ACHTUNG",$(el).closest( ".filter-entry" ).prop("checked"));
+    console.log("ACCCHHCH",$(el.parentElement).find(`.filter-entry[varc='${$(el).attr("varc")}']`).prop("checked"))
     saveToRepo('text',"Filter-text-"+ $(el).attr("varc"),encodeURIComponent($(el).prop("value")));
     restoreTexts();
   })
