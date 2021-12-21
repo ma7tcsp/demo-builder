8dfe5b068886ce3177f95f20881839c3580925ff
aalteirac <anthony@alteirac.com>
2021-12-21 14:58:22 +0100

    v1.4.3, widget template, new move handle

--- build-it.sh
+++ build-it.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 node prepdeploy.js
 git add .
-git commit -m "v1.4.2, widget template with management"
+git commit -m "v1.4.3, widget template, new move handle"
 git push
 docker stop testemb 
 docker rm testemb 
