8831e1f92bb6abe570940c56cd1cf54421c8cffe
aalteirac <anthony@alteirac.com>
2021-12-29 15:07:24 +0100

    removed files

--- prepdeploy.js
+++
@@ -1,9 +0,0 @@
-var compressor = require('node-minify');
- 
-// Using Google Closure Compiler
-compressor.minify({
-  compressor: 'gcc',
-  input: './public/devcenter/main.js',
-  output: './public/devcenter/main.min.js',
-  callback: function(err, min) {}
-});
