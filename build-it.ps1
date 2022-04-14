e75315b9e5d0afdc53c9fe717272369b95bd682a
ma7tcsp <ma7tcsp@gmail.com>
2022-04-14 10:58:53 +0100

    v1.6.0, issues with images

--- build-it.ps1
+++ build-it.ps1
@@ -2,11 +2,11 @@ node .\prepdeploy.js
 git add .
 git commit -m "v1.6.0, issues with images"
 git push
-rem docker stop testemb 
-rem docker rm testemb 
-rem docker image rm ma7tcsp/emb
+docker stop testemb 
+docker rm testemb 
+docker image rm ma7tcsp/emb
 docker build . -t ma7tcsp/emb
 docker run -p 3001:3000 -d --name testemb ma7tcsp/emb
 docker push ma7tcsp/emb
 npm run dist
-rem electron-builder --windows nsis:x64
+# electron-builder --windows nsis:x64
