2c23271590c601c0cca7bb67f3c7ff68a6edbc3f
ma7tcsp <ma7tcsp@gmail.com>
2022-04-14 10:54:12 +0100

    v1.6.0, issues with images

--- build-it.ps1
+++ build-it.ps1
@@ -6,7 +6,7 @@ rem docker stop testemb
 rem docker rm testemb 
 rem docker image rm ma7tcsp/emb
 docker build . -t ma7tcsp/emb
-docker run -p 80:3000 -d --name testemb ma7tcsp/emb
+docker run -p 3001:3000 -d --name testemb ma7tcsp/emb
 docker push ma7tcsp/emb
 npm run dist
-electron-builder --windows nsis:x64
+rem electron-builder --windows nsis:x64
